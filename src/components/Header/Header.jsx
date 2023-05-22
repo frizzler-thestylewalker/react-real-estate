de9c2969358d1273da78e28b7d58481316af13c7
Hrishikesh <hrishimane.22.rm@gmail.com>
2023-05-22 22:06:56 +0530

    header hero companies section added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <section className="h-wrapper">
+      <div className="flexCenter paddings innerwidth h-container">
+        <img src="./logo.png" alt="log" width={100} />
+
+        <div className="flexCenter h-menu">
+          <a href="">Residencies</a>
+          <a href="">Our Value</a>
+          <a href="">Contact Us</a>
+          <a href="">Get Started</a>
+          <button className="button">
+            <a href="">Contact</a>
+          </button>
+        </div>
+
+        <img src="" alt="" />
+      </div>
+    </section>
+  );
+}
+
+export default Header;

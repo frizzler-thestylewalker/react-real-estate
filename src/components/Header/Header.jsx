4389d58dc1467baba165137706d6d8cee36ae4cc
Hrishikesh <hrishimane.22.rm@gmail.com>
2023-05-24 22:37:26 +0530

    responsiveness, framer motion added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
+import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler/build/OutsideClickHandler";
 
 function Header() {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const getMenuStyles = (menuOpened) => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: !menuOpened && "-100%" };
+    }
+  };
+
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="log" width={100} />
 
-        <div className="flexCenter h-menu">
-          <a href="#Residencies">Residencies</a>
-          <a href="#value">Our Value</a>
-          <a href="#Contacts">Contact Us</a>
-          <a href="">Get Started</a>
-          <button className="button">
-            <a href="">Contact</a>
-          </button>
-        </div>
+        <OutsideClickHandler onOutsideClick={() => setMenuOpened(false)}>
+          <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
+            <a href="#Residencies">Residencies</a>
+            <a href="#value">Our Value</a>
+            <a href="#Contacts">Contact Us</a>
+            <a href="">Get Started</a>
+            <button className="button">
+              <a href="">Contact</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
 
-        <img src="" alt="" />
+        <div
+          className="menu-icon"
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <BiMenuAltRight src={30} />
+        </div>
       </div>
     </section>
   );
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 
 function Hero() {
   return (
@@ -12,11 +13,18 @@ function Hero() {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-            <h1>
+            <motion.h1
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                type: "spring",
+              }}
+            >
               Discover <br />
               Most Suitable <br />
               Property
-            </h1>
+            </motion.h1>
           </div>
 
           <div className="flexColStart hero-decription">
@@ -63,10 +71,19 @@ function Hero() {
 
         {/* right-side */}
 
-        <div className="hero-right">
-          <div className="image-container">
+        <div className="flexCenter hero-right">
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ rotate: 360, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 20,
+            }}
+            className="image-container"
+          >
             <img src="./hero-image.png" alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
